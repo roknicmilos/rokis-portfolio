c5760007e7a73f2be84c5cdadf443e6f73b2c363
Miloš Roknić <roknic.milos.994@gmail.com>
2024-08-10 17:37:25 +0200

    Add 'about me' and 'employment history' to CV

--- cv/views.py
+++ cv/views.py
@@ -29,6 +29,60 @@ class CVPDFView(PDFView):
         avatar_url = self.request.build_absolute_uri(
             f'{settings.STATIC_URL}cv/img/milos-roknic.png'
         )
+        about_me = (
+            'After completing programming courses and internships at two '
+            'companies, I secured a permanent Software Engineer role in the '
+            'second one, working primarily with PHP technologies. I later '
+            'transitioned to Python/Django and led two internal hackathon '
+            'projects in the past two years. I also gained experience with '
+            'Docker, CI tools, testing frameworks and mentored colleagues '
+            'and students in Python/Django and React. I also took on a '
+            'Development Lead role, managing six colleagues\' career '
+            'progression.'
+        )
+        employments = [
+            {
+                'title': 'Software Engineer & Development Lead',
+                'company': 'Vega IT',
+                'from': 'Aug 2023',
+                'until': 'Present',
+                'location': 'Novi Sad, Serbia',
+                'description': (
+                    'In Aug 2023, I assumed the role of Development Lead, '
+                    'taking on the responsibility of managing the career '
+                    'progression and work satisfaction of six colleagues. '
+                    'In addition the mentioned duties, I continued to work on '
+                    'Python/Django and Next.js projects while also gaining '
+                    'significant experience with Cypress in one of my latest '
+                    'projects. I also continued mentoring colleagues and '
+                    'students in Python/Django and React, while my experience '
+                    'with Docker and various CI tools has further grown '
+                    'during this period.'
+                )
+            },
+            {
+                'title': 'Software Engineer',
+                'company': 'Vega IT',
+                'from': 'Jan 2019',
+                'until': 'Aug 2023',
+                'location': 'Novi Sad, Serbia',
+                'description': (
+                    'During this period, I began my journey as a Software '
+                    'Engineer, where I initially worked extensively with PHP '
+                    'technologies, including Drupal, WordPress, and Symfony. '
+                    'After the first one and a half years, I transitioned to '
+                    'working on Python/Django and Next.js projects, a focus '
+                    'that spanned over four years. In addition to my '
+                    'development work, I took on leadership roles in two '
+                    'internal hackathon projects and mentored colleagues and '
+                    'students in Python/Django and React. I also gained '
+                    'valuable experience with Docker and various CI tools. '
+                    'For the last three years of this period, I exclusively '
+                    'worked on Ubuntu, solidifying my proficiency with this '
+                    'operating system.'
+                )
+            }
+        ]
         return {
             'avatar_url': avatar_url,
             'first_name': 'Miloš',
@@ -40,4 +94,6 @@ class CVPDFView(PDFView):
                 'label': 'Kornelija Stankovića 39, Novi Sad, 21000, Serbia',
                 'url': address_url
             },
+            'about_me': about_me,
+            'employments': employments,
         }
