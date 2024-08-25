e1e996b8280ac4197b8cdf4ec6d89758bd70bcca
Miloš Roknić <roknic.milos.994@gmail.com>
2024-08-25 17:54:11 +0200

    Remove 'cv/' parent path from CV URL paths

--- rokis_corner/urls.py
+++ rokis_corner/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cv/<slug:slug>/', include('cv.urls', namespace='cv')),
+    path('<slug:slug>/', include('cv.urls', namespace='cv')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
