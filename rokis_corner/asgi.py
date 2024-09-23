f3876fab026a8b54e1978f1b26c345c429e2eb37
Miloš Roknić <roknic.milos.994@gmail.com>
2024-09-23 22:39:06 +0200

    Split settings into 'main' and 'test'

--- rokis_corner/asgi.py
+++ rokis_corner/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rokis_corner.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rokis_corner.settings.main")
 
 application = get_asgi_application()
