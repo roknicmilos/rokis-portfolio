db65279c89722d40fb3343f6d423fbf218381b7b
Miloš Roknić <roknic.milos.994@gmail.com>
2024-10-10 22:27:05 +0200

    Add tests for UserManager

--- apps/user/models/user.py
+++ apps/user/models/user.py
@@ -24,22 +24,17 @@ class UserManager(BaseUserManager):
         """
         Overridden from base manager to create a user without username.
         """
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email=None, password=None, **extra_fields):
         """
         Overridden from base manager to create a superuser without username.
         """
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
+        extra_fields.update({
+            "is_active": True,
+            "is_staff": True,
+            "is_superuser": True,
+        })
         return self._create_user(email, password, **extra_fields)
 
 
