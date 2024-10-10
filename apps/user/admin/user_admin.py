224cff946fc0700ad76736c0b13fac9004062c49
Miloš Roknić <roknic.milos.994@gmail.com>
2024-10-10 23:09:15 +0200

    Adjust UserAdmin for non-superusers

--- apps/user/admin/user_admin.py
+++ apps/user/admin/user_admin.py
@@ -13,12 +13,15 @@ admin.site.unregister(Group)
 class UserAdmin(BaseUserAdmin):
     list_display = (
         "email",
-        "is_active",
-        "is_staff",
-        "is_superuser",
         "portfolio_count",
         "date_joined",
         "last_login",
+    )
+    superuser_list_display = (
+        *list_display,
+        "is_active",
+        "is_staff",
+        "is_superuser",
     )
     readonly_fields = (
         "portfolio_count",
@@ -27,40 +30,49 @@ class UserAdmin(BaseUserAdmin):
         "date_joined",
         "last_login",
     )
+    main_fieldset = (None, {"fields": ("email", "password")})
+    personal_info_fieldset = (
+        _("Personal info"),
+        {
+            "fields": (
+                "first_name",
+                "last_name",
+            )
+        },
+    )
+    permissions_fieldset = (
+        _("Permissions"),
+        {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            ),
+        },
+    )
+    important_dates_fieldset = (
+        _("Important dates"),
+        {
+            "fields": (
+                "last_login",
+                "date_joined",
+                "created",
+                "modified",
+            )
+        },
+    )
     fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        (
-            _("Personal info"),
-            {
-                "fields": (
-                    "first_name",
-                    "last_name",
-                )
-            },
-        ),
-        (
-            _("Permissions"),
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
-                ),
-            },
-        ),
-        (
-            _("Important dates"),
-            {
-                "fields": (
-                    "last_login",
-                    "date_joined",
-                    "created",
-                    "modified",
-                )
-            },
-        ),
+        main_fieldset,
+        personal_info_fieldset,
+        important_dates_fieldset,
+    )
+    superuser_fieldsets = (
+        main_fieldset,
+        personal_info_fieldset,
+        permissions_fieldset,
+        important_dates_fieldset,
     )
     add_fieldsets = (
         (
@@ -73,3 +85,28 @@ class UserAdmin(BaseUserAdmin):
     )
     search_fields = ("first_name", "last_name", "email")
     ordering = ("email",)
+
+    def get_queryset(self, request):
+        if not request.user.is_superuser:
+            return User.objects.filter(id=request.user.id)
+        return super().get_queryset(request)
+
+    def get_list_display(self, request):
+        if request.user.is_superuser:
+            return self.superuser_list_display
+        return self.list_display
+
+    def get_fieldsets(self, request, obj=None):
+        if request.user.is_superuser:
+            return self.superuser_fieldsets
+        return self.fieldsets
+
+    def get_search_fields(self, request):
+        if request.user.is_superuser:
+            return self.search_fields
+        return ()
+
+    def get_list_filter(self, request):
+        if request.user.is_superuser:
+            return self.list_filter
+        return ()
