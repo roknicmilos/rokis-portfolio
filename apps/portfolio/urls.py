fcbdf52739081fe88536c5372865607a429702de
Miloš Roknić <roknic.milos.994@gmail.com>
2024-10-15 22:43:47 +0200

    Style registration page

--- apps/portfolio/urls.py
+++ apps/portfolio/urls.py
@@ -5,18 +5,10 @@ from apps.portfolio.views import PortfolioPDFView
 app_name = "portfolio"
 
 urlpatterns = [
+    path("", PortfolioPDFView.as_view(response_type="html"), name="index"),
+    path("pdf/", PortfolioPDFView.as_view(response_type="pdf"), name="pdf"),
     path(
-        "<slug:slug>/",
-        PortfolioPDFView.as_view(response_type="html"),
-        name="index",
-    ),
-    path(
-        "<slug:slug>/pdf/",
-        PortfolioPDFView.as_view(response_type="pdf"),
-        name="pdf",
-    ),
-    path(
-        "<slug:slug>/download/",
+        "download/",
         PortfolioPDFView.as_view(response_type="download"),
         name="download",
     ),
